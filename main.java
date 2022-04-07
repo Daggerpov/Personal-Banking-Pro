4bccd56674c21792dd185b16f5cc8e95a4b2df34
Daggerpov <danielagapov1@gmail.com>
2022-04-07 09:56:15 -0400

    basic template used to create first frame

---
+++ main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+class gui {
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
+    }
+}
