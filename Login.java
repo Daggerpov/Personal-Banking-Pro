d03bbcdd4da2155f663a58957620b1bb80425e9d
Daggerpov <danielagapov1@gmail.com>
2022-04-16 13:32:04 -0400

    making login screen with basic dimensions, need to edit UI around it

---
+++ Login.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login {
+    public static void main(String[] a) {
+        LoginFrame frame = new LoginFrame();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 570, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+    }
+
+}
---
+++ LoginFrame.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends JFrame implements ActionListener {
+
+	Container container = getContentPane();
+	JLabel userLabel = new JLabel("USERNAME");
+	JLabel passwordLabel = new JLabel("PASSWORD");
+	JTextField userTextField = new JTextField();
+	JPasswordField passwordField = new JPasswordField();
+	JButton loginButton = new JButton("LOGIN");
+	JButton resetButton = new JButton("RESET");
+	JCheckBox showPassword = new JCheckBox("Show Password");
+
+	LoginFrame() {
+		// Calling methods inside constructor.
+		setLayoutManager();
+		setLocationAndSize();
+		addComponentsToContainer();
+
+	}
+
+	public void setLayoutManager() {
+		container.setLayout(null);
+	}
+
+	public void setLocationAndSize() {
+		// Setting location and Size of each components using setBounds() method.
+		userLabel.setBounds(50, 150, 100, 30);
+		passwordLabel.setBounds(50, 220, 100, 30);
+		userTextField.setBounds(150, 150, 150, 30);
+		passwordField.setBounds(150, 220, 150, 30);
+		showPassword.setBounds(150, 250, 150, 30);
+		loginButton.setBounds(50, 300, 100, 30);
+		resetButton.setBounds(200, 300, 100, 30);
+
+	}
+
+	public void addComponentsToContainer() {
+		// Adding each components to the Container
+		container.add(userLabel);
+		container.add(passwordLabel);
+		container.add(userTextField);
+		container.add(passwordField);
+		container.add(showPassword);
+		container.add(loginButton);
+		container.add(resetButton);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
