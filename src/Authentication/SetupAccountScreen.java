a9723efa3f73a20ab25c76d77543d5af50213fc8
Jasman04 <jsang1@ocdsb.ca>
2022-04-17 21:14:18 -0400

    Made navigation buttons for some accounts

--- src/Authentication/SetupAccountScreen.java
+++ src/Authentication/SetupAccountScreen.java
@@ -7,24 +7,26 @@ import java.awt.event.ActionListener;
 public class SetupAccountScreen extends JFrame implements ActionListener {
 
     Container container = getContentPane();
-    JLabel titleLabel = new JLabel("It's time to pretend you're a real user with money!");
-    JLabel fNameLabel = new JLabel("What is your full name?");
-    JLabel usernameLabel = new JLabel("What would you like your username to be?");
-    JTextField fNameTextField = new JTextField();
-    JTextField usernameTextField = new JTextField();
-    JPasswordField passwordField = new JPasswordField();
-    JLabel passwordLabel = new JLabel("Password:");
-    JLabel confirmPasswordLabel = new JLabel("Confirm Password:");
-    JTextField confirmPasswordTextField = new JTextField();
-    JCheckBox showPassword = new JCheckBox("Show Password");
-    JButton setupAccountButton = new JButton("Setup Account");
+    JLabel savingsLabel = new JLabel("Creae Savings Account");
+    JCheckBox savingsBox = new JCheckBox();
+    JLabel checkingsLabel = new JLabel("Create Checking Account");
+    JCheckBox checkingsBox = new JCheckBox();
+    JLabel investmentLabel = new JLabel("Create Investment Account");
+    JCheckBox investmentBox = new JCheckBox();
+    JLabel savingsBalanceLabel = new JLabel("Balance:");
+    JTextField savingsTextField = new JTextField();
+    JLabel checkingsBalanceLabel = new JLabel("Balance:");
+    JTextField checkingsTextField = new JTextField();
+    JLabel investmentsBalanceLabel = new JLabel("Balance:");
+    JTextField investmentsTextField = new JTextField();
+    JButton nextButton = new JButton("Next");
 
     SetupAccountScreen() {
         // Calling methods inside constructor.
         setLayoutManager();
         setLocationAndSize();
         addComponentsToContainer();
-        addActionEvent();
+        //addActionEvent();
     }
 
     public void setLayoutManager() {
@@ -33,51 +35,47 @@ public class SetupAccountScreen extends JFrame implements ActionListener {
 
     public void setLocationAndSize() {
         // Setting location and Size of each components using setBounds() method.
-        titleLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-        titleLabel.setBounds(85, 20, 600, 30);
 
-        fNameLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-        fNameLabel.setBounds(10, 80, 500, 30);
-        fNameTextField.setBounds(225, 80, 150, 30);
+        savingsLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+        savingsLabel.setBounds(10, 50, 500, 30);
+        savingsBox.setBounds(225, 50, 150, 30);
+        
 
-        usernameLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-        usernameLabel.setBounds(10, 140, 500, 30);
-        usernameTextField.setBounds(375, 140, 150, 30);
+        checkingsLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+        checkingsLabel.setBounds(10, 140, 500, 30);
+        checkingsBox.setBounds(375, 140, 150, 30);
 
-        passwordLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-        passwordLabel.setBounds(10, 200, 100, 30);
-        passwordField.setBounds(110, 200, 150, 30);
+        investmentLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+        investmentLabel.setBounds(10, 200, 100, 30);
+        investmentBox.setBounds(110, 200, 150, 30);
 
-        confirmPasswordLabel.setFont(new Font("Serif", Font.PLAIN, 20));
-        confirmPasswordLabel.setBounds(10, 260, 200, 40);
-        confirmPasswordTextField.setBounds(175, 263, 150, 30);
 
-        showPassword.setFont(new Font("Serif", Font.PLAIN, 16));
-        showPassword.setBounds(175, 300, 150, 30);
 
-        setupAccountButton.setFont(new Font("Serif", Font.PLAIN, 20));
-        setupAccountButton.setBounds(225, 350, 150, 40);
+        nextButton.setFont(new Font("Serif", Font.PLAIN, 20));
+        nextButton.setBounds(10, 500, 50, 40);
 
     }
 
     public void addComponentsToContainer() {
         // Adding each components to the Container
-        container.add(titleLabel);
-        container.add(passwordLabel);
-        container.add(fNameLabel);
-        container.add(usernameLabel);
-        container.add(fNameTextField);
-        container.add(usernameTextField);
-        container.add(showPassword);
-        container.add(confirmPasswordLabel);
-        container.add(passwordField);
-        container.add(confirmPasswordTextField);
-        container.add(setupAccountButton);
+        container.add(investmentLabel);
+        container.add(savingsLabel);
+        container.add(checkingsLabel);
+        container.add(savingsBox);
+        container.add(checkingsBox);
+        container.add(investmentBox);
+        container.add(investmentsBalanceLabel);
+        container.add(savingsBalanceLabel);
+        container.add(checkingsBalanceLabel);
+        container.add(checkingsTextField);
+        container.add(savingsTextField);
+        container.add(investmentsTextField);
+        container.add(nextButton);
     }
 
-    public void addActionEvent() {
-        showPassword.addActionListener(this);
-    }
+    //public void addActionEvent() {
+    //    showPassword.addActionListener(this);
+    //}
 
     @Override
     public void actionPerformed(ActionEvent e) {
