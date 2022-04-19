e729fed4557fa5fd313f3617ed458b5d59830085
Daggerpov <danielagapov1@gmail.com>
2022-04-18 22:13:24 -0400

    setting up individual screen launch for Jasman

--- src/main.java
+++ src/main.java
@@ -2,19 +2,32 @@ package src;
 
 import javax.swing.*;
 import src.Authentication.LoginScreen;
+import src.Authentication.SetupAccountScreen;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class main {
     public static void main(String[] a) {
-        LoginScreen frame = new LoginScreen();
-        frame.setVisible(true);
-        frame.setTitle("Login");
+        // LoginScreen frame = new LoginScreen();
+        // frame.setVisible(true);
+        // frame.setTitle("Login");
+        // frame.setVisible(true);
+        // frame.setBounds(10, 10, 600, 600);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //change these names to the names of the classes such as ____Screen accordingly
+        //! only thing to keep in mind is that if it's underlined in red
+        //! that means you need to hover over it -> quick fix -> import ___Screen
+        SetupAccountScreen frame = new SetupAccountScreen();
+        frame.setTitle("Setup Account");
         frame.setVisible(true);
         frame.setBounds(10, 10, 600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
     }
 
